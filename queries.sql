ddb7b5cf8400f84a2a3832bf6980c24cd1837346
Francisca Escamilla <francis.escamilla@gmail.com>
2021-10-02 21:31:03 -0500

    1st notebook is complete.

---
+++ queries.sql
@@ -0,0 +1,11 @@
+select count(*) from review_id_table; 
+
+select * 
+from review_id_table
+Where review_date = '2015-08-31';
+
+select count(*) from products; 
+
+select count(*) from customers; 
+
+select count(*) from vine_table; 
